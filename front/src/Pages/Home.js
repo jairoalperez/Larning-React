ab5e43aa503f2d002b4dfbf4d9baa831e0583744
jairoalperez <jairoalberto98@hotmail.com>
2023-01-25 11:24:18 -0500

    a

---
+++ front/src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserLogin from '../Components/UserLogin'
+
+function Home() {
+  return (
+    <UserLogin userEmail='jairoalperez2001@gmail.com' userName='Messi'/>
+  )
+}
+
+export default Home
